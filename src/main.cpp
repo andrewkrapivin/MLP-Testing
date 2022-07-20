9c193586f3539f1eaa836a68ac24cc7a2f660d43
Andrew Krapivin <andrewakrapivin@gmail.com>
2022-07-20 14:26:05 -0400

    Implemented a simple tester for the amount of time it takes to load data, either sequentially or using random reads. Obviously the sequential speed is faster because of multiple elements in a cache line, but that's just there for reference.

---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+#include "BenchHelper.hpp"
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void readSequential(vector<vector<size_t>>& arrays) {
+    for(auto& array: arrays) {
+        for(auto& x: array) {
+            (void)x;
+        }
+    }
+}
+
+//Assuming all the arrays have the same size!!
+void readRandom(vector<vector<size_t>>& arrays, bool usePrefetch = true) {
+    vector<size_t> pointers(arrays.size()); //should be default init to zero
+    for(size_t i{0}; i < arrays[0].size(); i++) {
+        if(usePrefetch) {
+            for(size_t j{0}; j < arrays.size(); j++) {
+                __builtin_prefetch(&arrays[j][pointers[j]]);
+            }
+        }
+        for(size_t j{0}; j < arrays.size(); j++) {
+            pointers[j] = arrays[j][pointers[j]];
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    unsigned seed = chrono::steady_clock::now().time_since_epoch().count();
+    mt19937 generator (seed);
+
+    size_t testSize = 10;
+    size_t numPointerChases = 1;
+    if(argc >= 2)
+        testSize = atoi(argv[1]);
+    if(argc >= 3) 
+        numPointerChases = atoi(argv[2]);
+
+    vector<vector<size_t>> arrays(numPointerChases, vector<size_t>(testSize));
+    for(auto& array: arrays) {
+        for(size_t i{0}; i < array.size(); i++) {
+            array[i] = i;
+        }
+        shuffle(array.begin(), array.end(), generator);
+    }
+
+    BenchHelper bench;
+
+    bench.timeFunction([&] ()-> void {
+        readSequential(arrays);
+    }, "sequential read time");
+
+    bench.timeFunction([&] () -> void {
+        readRandom(arrays);
+    }, "random read time");
+
+    bench.timeFunction([&] () -> void {
+        readRandom(arrays, false);
+    }, "random read time without prefetch");
+
+}
