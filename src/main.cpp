bbe743b375a4ff0019c247e66040a9f6ee8e97da
Andrew Krapivin <andrewakrapivin@gmail.com>
2022-07-20 22:49:41 -0400

    Made it so that it displays results of all the possibilities for number of simultaneous pointer chases, from 1 to the number specified in command line args. Not sure if that's actually helpful. Also added full cacheline just to sanity check because I had some seemingly weird results with that earlier.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,42 @@
 #include "BenchHelper.hpp"
 #include <iostream>
 #include <algorithm>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <cassert>
 
 using namespace std;
 
-void readSequential(vector<vector<size_t>>& arrays) {
+struct alignas(64) CacheLine {
+    size_t data;
+    size_t filler[7];
+};
+
+template <typename T> struct thp_allocator {
+  constexpr static std::size_t huge_page_size = 1 << 21; // 2 MiB
+  using value_type = T;
+
+  thp_allocator() = default;
+  template <class U>
+  constexpr thp_allocator(const thp_allocator<U> &) noexcept {}
+
+  T *allocate(std::size_t n) {
+    if (n > std::numeric_limits<std::size_t>::max() / sizeof(T)) {
+      throw std::bad_alloc();
+    }
+    void *p = nullptr;
+    posix_memalign(&p, huge_page_size, n * sizeof(T));
+    madvise(p, n * sizeof(T), MADV_HUGEPAGE);
+    if (p == nullptr) {
+      throw std::bad_alloc();
+    }
+    return static_cast<T *>(p);
+  }
+
+  void deallocate(T *p, std::size_t n) { std::free(p); }
+};
+
+void readSequential(vector<vector<size_t, thp_allocator<size_t>>>& arrays) {
     for(auto& array: arrays) {
         for(auto& x: array) {
             (void)x;
@@ -13,16 +45,32 @@ void readSequential(vector<vector<size_t>>& arrays) {
 }
 
 //Assuming all the arrays have the same size!!
-void readRandom(vector<vector<size_t>>& arrays, bool usePrefetch = true) {
+void readRandom(vector<vector<size_t, thp_allocator<size_t>>>& arrays, size_t numSimultaneousChases = 1, bool usePrefetch = true) {
+    assert(numSimultaneousChases <= arrays.size());
     vector<size_t> pointers(arrays.size()); //should be default init to zero
     for(size_t i{0}; i < arrays[0].size(); i++) {
         if(usePrefetch) {
-            for(size_t j{0}; j < arrays.size(); j++) {
+            for(size_t j{0}; j < numSimultaneousChases; j++) {
                 __builtin_prefetch(&arrays[j][pointers[j]]);
             }
         }
-        for(size_t j{0}; j < arrays.size(); j++) {
+        for(size_t j{0}; j < numSimultaneousChases; j++) {
             pointers[j] = arrays[j][pointers[j]];
+        }
+    }
+}
+
+void readRandomCacheline(vector<vector<CacheLine, thp_allocator<CacheLine>>>& arrays, size_t numSimultaneousChases = 1, bool usePrefetch = true) {
+    assert(numSimultaneousChases <= arrays.size());
+    vector<CacheLine> pointers(arrays.size()); //should be default init to zero
+    for(size_t i{0}; i < arrays[0].size(); i++) {
+        if(usePrefetch) {
+            for(size_t j{0}; j < numSimultaneousChases; j++) {
+                __builtin_prefetch(&arrays[j][pointers[j].data]);
+            }
+        }
+        for(size_t j{0}; j < numSimultaneousChases; j++) {
+            pointers[j] = arrays[j][pointers[j].data];
         }
     }
 }
@@ -38,11 +86,19 @@ int main(int argc, char** argv) {
     if(argc >= 3) 
         numPointerChases = atoi(argv[2]);
 
-    vector<vector<size_t>> arrays(numPointerChases, vector<size_t>(testSize));
-    vector<vector<size_t>> singlePointerChaseComparison(1, vector<size_t>(numPointerChases*testSize));
+    vector<vector<size_t, thp_allocator<size_t>>> arrays(numPointerChases, vector<size_t, thp_allocator<size_t>>(testSize));
+    vector<vector<CacheLine, thp_allocator<CacheLine>>> arraysFullCacheline(numPointerChases, vector<CacheLine, thp_allocator<CacheLine>>(testSize));
+    vector<vector<size_t, thp_allocator<size_t>>> singlePointerChaseComparison(1, vector<size_t, thp_allocator<size_t>>(testSize));
     for(auto& array: arrays) {
         for(size_t i{0}; i < array.size(); i++) {
             array[i] = i;
+        }
+        shuffle(array.begin(), array.end(), generator);
+    }
+
+    for(auto& array: arraysFullCacheline) {
+        for(size_t i{0}; i < array.size(); i++) {
+            array[i].data = i;
         }
         shuffle(array.begin(), array.end(), generator);
     }
@@ -58,16 +114,32 @@ int main(int argc, char** argv) {
         readSequential(arrays);
     }, "sequential read time");
 
-    bench.timeFunction([&] () -> void {
-        readRandom(arrays);
-    }, "random read time");
+    for(size_t i{1}; i <= numPointerChases; i++) {
+        bench.timeFunction([&] () -> void {
+            readRandom(arrays, i);
+        }, "random read time with " + to_string(i) + " simultaneous pointer chases");
+    }
+
+    for(size_t i{1}; i <= numPointerChases; i++) {
+        bench.timeFunction([&] () -> void {
+            readRandom(arrays, i, false);
+        }, "random read time without prefetch with " + to_string(i) + " simultaneous pointer chases");
+    }
+
+    for(size_t i{1}; i <= numPointerChases; i++) {
+        bench.timeFunction([&] () -> void {
+            readRandomCacheline(arraysFullCacheline, i);
+        }, "full cacheline random read time with " + to_string(i) + " simultaneous pointer chases");
+    }
+
+    for(size_t i{1}; i <= numPointerChases; i++) {
+        bench.timeFunction([&] () -> void {
+            readRandomCacheline(arraysFullCacheline, i, false);
+        }, "full cacheline random read time without prefetch with " + to_string(i) + " simultaneous pointer chases");
+    }
 
     bench.timeFunction([&] () -> void {
-        readRandom(arrays, false);
-    }, "random read time without prefetch");
-
-    bench.timeFunction([&] () -> void {
-        readRandom(singlePointerChaseComparison, false);
+        readRandom(singlePointerChaseComparison, 1, false);
     }, "random read time using a single pointer chase");
 
 }
